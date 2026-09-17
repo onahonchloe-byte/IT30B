e243e9992bbb67cc530898547476ab1e5def255a
onahonchloe-byte <onahonchloe@gmail.com>
2026-09-17 08:39:11 +0800

    add student information

---
+++ database/migrations/001_initial_library.sql
@@ -0,0 +1,85 @@
+
+CREATE TABLE IF NOT EXIST students (
+    --Primary key
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+
+    --Student name
+    student_first_name VARCHAR(50) NOT NULL,
+    student_last_name VARCHAR(50) NOT NULL,
+
+    --Student course 
+    student_course VARCHAR(50) NOT NULL,
+
+    --Student create at timestamp
+    student_created_at TIMESTAMP NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+
+) ENGINE=InnoDB;
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+--#2 books table 
+CREATE TABLE IF NOT EXIST books (
+    --Primary key
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    --Primary key for the books table
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+
+    --book details
+    book_title VARCHAR(100) NOT NULL,
+    book_author VARCHAR(100) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
+
+    --book create at timestamp
+    book_created_at TIMESTAMP NOT NULL
+    DEFAULT CURRENT_TIMESTAMP
+
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_unicode_ci;
+
+--#3 borrow table
+CREATE TABLE IF NOT EXIST borrow (
+    --Primary key for the borrow table
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    --Foreign key references
+    student_id INT NOT NULL,
+    book_id INT NOT NULL,
+
+    --Borrow timestamp not null by default
+    borrow_date TIMESTAMP NOT NULL 
+        DEFAULT CURRENT_TIMESTAMP,
+
+    borrow_return_date TIMESTAMP NULL,
+        DEFAULT NULL,
+
+    -- Borrow table constraints for foreign keys
+    CONSTRAINT fk_student_id 
+    FOREIGN KEY (student_id) 
+    REFERENCES students(student_id) 
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_borrow
+    FOREIGN KEY (book_id)
+    REFERENCES books(book_id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
+
+-- Insert statement #1: 
+INSERT INTO books (
+    book_title, 
+    book_author, 
+    book_category
+) VALUES
+('I love you since 1982', 'Binibini Mia', 'History-Romance'),
+('Taste of Sky', 'Armstrong', 'Sci-Fi-Romance'),
+('He into her', 'Maxinejiji', 'High School-Romance'),
+('The Greatest Showman', 'Hugh Jackman', 'Musical-Romance');
+
+-- Insert statement #2:
--- index.php
+++ index.php
@@ -21,5 +21,84 @@ try{
     die("Database connection failed".  $e->getMessage());
 }
 
+//Session
+session_start();
 
+//Determine current section
+$section =$_GET['section'] ??'student';
+
+//Determine CRUD Operation
+$action = $_GET['action'] ??'';
+
+    $stmt = $pdo->("
+    SELECT *
+    FROM students
+    ORDER BY student_id DESC
+    ");
+
+$students = $stmt->fetchAll();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library System</title>
+</head>
+<body>
+    <h1>Simple Library System</h1>
+    <nav>
+        <a href="index.php?section=students">Students</a>
+        <a href="index.php?section=books">Books</a>
+        <a href="index.php?section=borrow">borrow</a>
+    </nav>
+    <hr>
+    <?php if($section === 'students'): ?>
+        <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Create at</th>
+                    <th>Actions</th>
+                </tr>
+            <thead>
+        <tbody>
+            <?php foreach($students as student): ?>
+                <tr>
+                    <td>
+                        <?=htmlspecialchars($student['student_id']) ?>
+                    </td>
+                    <td>
+                        <?=htmlspecialchars($student['student_first_name']) ?>
+                    </td>
+                    <td>
+                        <?=htmlspecialchars($student['student_last_name']) ?>
+                    </td>
+                    <td>
+                        <?=htmlspecialchars($student['student_course']) ?>
+                    </td>
+                    <td>
+                        <?=htmlspecialchars($student['student_course_at']) ?>
+                    </td>
+                    <td>
+                        <a>Edit</a>
+                        <a>Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach?>
+        </tbody>
+
+    <?php endif;?>
+
+    <?php if($section === 'books'):?>
+        <h1>Books</h1>
+    <?php if ($section === 'borrow'): ?>
+        <h1>Borrow</h1>
+    <?php endif;?>
+</body>
+</html>
+
